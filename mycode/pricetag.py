d211a88891fceade20b8625efafeff15106e6f66
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2020-12-13 21:29:03 +0600

    First update

---
+++ mycode/pricetag.py
@@ -0,0 +1,10 @@
+price = 1000000
+is_GoodCredit = False
+
+if is_GoodCredit:
+    down_payment = 0.1 * price
+    
+    
+else:
+    down_payment = 0.2 * price
+print (f"Down payment: ${down_payment}")
